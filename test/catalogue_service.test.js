554249bf92cb0e67b7e47feedf9588deab8e6865
Saadiya Moid <saadiyamoid@gmail.com>
2019-12-02 17:01:22 +0000

    updates

--- test/catalogue_service.test.js
+++ test/catalogue_service.test.js
@@ -18,4 +18,18 @@ describe("catalogueService", () => {
       expect(catalogueService.checkBook("The Wind")).toBe(false);
     });
   });
+  describe("catalogueService.countBooksByKeyword", () => {
+    test("returns 3 when title contains assassin", () => {
+      expect(catalogueService.countBooksByKeyword("assassin")).toBe(3);
+    });
+    test("returns 2 when title contains normal", () => {
+      expect(catalogueService.countBooksByKeyword("normal")).toBe(2);
+    });
+    test("returns 0 when title does not contain pineapple", () => {
+      expect(catalogueService.countBooksByKeyword("pineapple")).toBe(0);
+    });
+    test("returns 1 when title does contains 6", () => {
+      expect(catalogueService.countBooksByKeyword("6")).toBe(1);
+    });
+  });
 });
